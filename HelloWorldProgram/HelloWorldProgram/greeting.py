c715c092c96df3694d9994d5591cb402a7b7ac78
Kamil Radziwon <kamil99radziweon@gmail.com>
2025-07-13 21:37:06 +0200

    nieudane próby wrpowadzenia testów

---
+++ HelloWorldProgram/HelloWorldProgram/greeting.py
@@ -0,0 +1,8 @@
+# HelloWorldProgram/HelloWorldProgram/greeting.py
+
+def greet(name: str, city: str) -> str:
+    """
+    Zwraca powitanie w formacie:
+    'Witaj, {name} z {city}!'
+    """
+    return f"Witaj, {name} z {city}!"
